4951ca92df35e6b4b4b7dfaebb9048f62e1ee75c
Aamir Shahzadah <aamirshahzadah@gmail.com>
2025-01-06 03:48:38 +0500

    cheking

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Optional: Example config option
-  };
-  
-  export default nextConfig;  // Use export default for ES Modules
-  
-  
+reactStrictMode: true,
+};
 
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
+export default nextConfig;
